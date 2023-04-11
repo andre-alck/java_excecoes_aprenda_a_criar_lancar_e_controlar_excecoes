3f6b4da5827da4efe38af02497ab3faad2eb8a6d
andre-alck <andrealck1@gmail.com>
2023-04-10 22:51:19 -0300

    feat: add 02

--- java-pilha/Fluxo.java
+++ java-pilha/Fluxo.java
@@ -1,21 +1,37 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Fluxo {
     public static void main(String[] args) {
         System.out.println("Início do main");
-        metodo1();
+
+        try {
+            metodo1();
+        } catch (ArithmeticException | NullPointerException exception) {
+            System.out.println("Exceção capturada | " + exception.getMessage() + " | " + Arrays.stream(exception.getStackTrace()).collect(Collectors.toList()));
+        }
+
         System.out.println("Fim do main");
     }
 
     private static void metodo1() {
         System.out.println("Início do metodo1");
+
         metodo2();
+
         System.out.println("Fim do metodo1");
     }
 
     private static void metodo2() {
         System.out.println("Início do metodo2");
-        for(int i = 0; i < 5; i++) {
+
+        for (int i = 0; i < 5; i++) {
             System.out.println(i);
+
+            Conta conta = null;
+            conta.deposita();
         }
+
         System.out.println("Fim do metodo2");
     }
 }
